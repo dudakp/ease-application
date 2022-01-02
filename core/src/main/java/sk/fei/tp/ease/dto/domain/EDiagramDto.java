110691160d811a029cea5f314a88e3182acdec06
dudakp <palodudak@gmail.com>
2022-01-03 00:44:20 +0100

    opravene pomenovania accessor metod v dto

--- core/src/main/java/sk/fei/tp/ease/dto/domain/EDiagramDto.java
+++ core/src/main/java/sk/fei/tp/ease/dto/domain/EDiagramDto.java
@@ -1,81 +1,16 @@
 package sk.fei.tp.ease.dto.domain;
 
+import lombok.Data;
+
 import java.util.List;
-import java.util.Objects;
 
+@Data
 public final class EDiagramDto {
-    private final Long id;
-    private final EPackageDto tPackage;
-    private final List<EObjectDto> objects;
-    private final String name;
-    private final String author;
-    private final Integer version;
-
-    public EDiagramDto(Long id,
-                       EPackageDto tPackage,
-                       List<EObjectDto> objects,
-                       String name,
-                       String author,
-                       Integer version) {
-        this.id = id;
-        this.tPackage = tPackage;
-        this.objects = objects;
-        this.name = name;
-        this.author = author;
-        this.version = version;
-    }
-
-    public Long id() {
-        return id;
-    }
-
-    public EPackageDto tPackage() {
-        return tPackage;
-    }
-
-    public List<EObjectDto> objects() {
-        return objects;
-    }
-
-    public String name() {
-        return name;
-    }
-
-    public String author() {
-        return author;
-    }
-
-    public Integer version() {
-        return version;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (EDiagramDto) obj;
-        return Objects.equals(this.id, that.id) &&
-                Objects.equals(this.tPackage, that.tPackage) &&
-                Objects.equals(this.objects, that.objects) &&
-                Objects.equals(this.name, that.name) &&
-                Objects.equals(this.author, that.author) &&
-                Objects.equals(this.version, that.version);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, tPackage, objects, name, author, version);
-    }
-
-    @Override
-    public String toString() {
-        return "EDiagramDto[" +
-                "id=" + id + ", " +
-                "tPackage=" + tPackage + ", " +
-                "objects=" + objects + ", " +
-                "name=" + name + ", " +
-                "author=" + author + ", " +
-                "version=" + version + ']';
-    }
+    private Long id;
+    private EPackageDto tPackage;
+    private List<EObjectDto> objects;
+    private String name;
+    private String author;
+    private Integer version;
 
 }
