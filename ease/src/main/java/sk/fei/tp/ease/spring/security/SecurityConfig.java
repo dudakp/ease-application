edfc045be9d64abe47bd6b847d149de4cfff6d6c
dudakp <palodudak@gmail.com>
2022-01-03 19:19:39 +0100

    cors

--- ease/src/main/java/sk/fei/tp/ease/spring/security/SecurityConfig.java
+++ ease/src/main/java/sk/fei/tp/ease/spring/security/SecurityConfig.java
@@ -1,19 +1,32 @@
 package sk.fei.tp.ease.spring.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authorizeRequests -> authorizeRequests
+        http.cors().and().csrf().disable()
+                .authorizeRequests(authorizeRequests -> authorizeRequests
                 .antMatchers("/actuator/**").permitAll()
                 .antMatchers("/v3/**", "/api-docs/**", "/swagger/**").permitAll()
                 .anyRequest().authenticated()
         )
                 .oauth2ResourceServer().jwt();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 }
