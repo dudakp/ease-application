29381cf9affe73ab5dae89c055ec667b62bd600b
dudakp <palodudak@gmail.com>
2022-01-03 19:27:33 +0100

    cors pokus 2

--- ease/src/main/java/sk/fei/tp/ease/spring/security/SecurityConfig.java
+++ ease/src/main/java/sk/fei/tp/ease/spring/security/SecurityConfig.java
@@ -9,12 +9,21 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.List;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+
+        http.cors().configurationSource(req->corsConfiguration).and().csrf().disable()
                 .authorizeRequests(authorizeRequests -> authorizeRequests
                 .antMatchers("/actuator/**").permitAll()
                 .antMatchers("/v3/**", "/api-docs/**", "/swagger/**").permitAll()
