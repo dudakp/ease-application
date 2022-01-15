363a2cdd2665beb26bd0da85bb60263b75dacfe9
dudakp <palodudak@gmail.com>
2022-01-15 17:51:00 +0100

    cors

--- ease/src/main/java/sk/fei/tp/ease/spring/security/SecurityConfig.java
+++ ease/src/main/java/sk/fei/tp/ease/spring/security/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost", "https://tp2-ai.fei.stuba.sk"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setExposedHeaders(List.of("Authorization"));
