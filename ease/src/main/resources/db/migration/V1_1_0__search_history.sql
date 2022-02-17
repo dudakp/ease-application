ee8a2e814799139ee09fbb19c701efabc96faf58
dudakp <palodudak@gmail.com>
2022-02-17 11:42:44 +0100

    oprava sql migracie pre historiu vyhladavania

--- ease/src/main/resources/db/migration/V1_1_0__search_history.sql
+++ ease/src/main/resources/db/migration/V1_1_0__search_history.sql
@@ -1,8 +1,7 @@
-CREATE TABLE `search_history` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`user` varchar(255) COLLATE utf8_slovak_ci NOT NULL,
-	`time` timestamp NOT NULL,
-	`searched_expression` varchar(255) COLLATE utf8_slovak_ci NOT NULL,
-	PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovak_ci	
-
+CREATE TABLE "search_history"
+(
+    "id"                  INT PRIMARY KEY NOT NULL,
+    "user"                varchar(255)    NOT NULL,
+    "time"                timestamp       NOT NULL,
+    "searched_expression" varchar(255)    NOT NULL
+)
--- user/src/main/java/sk/fei/tp/ease/rest/UserController.java
+++ user/src/main/java/sk/fei/tp/ease/rest/UserController.java
@@ -1,10 +1,7 @@
 package sk.fei.tp.ease.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/user")
