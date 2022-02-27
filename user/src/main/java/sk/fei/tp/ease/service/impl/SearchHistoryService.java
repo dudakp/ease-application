1f3ae140e824167cfb69a46f52c17615ddad6b41
dudakp <palodudak@gmail.com>
2022-02-27 20:12:05 +0100

    feat: userUtils

--- user/src/main/java/sk/fei/tp/ease/service/impl/SearchHistoryService.java
+++ user/src/main/java/sk/fei/tp/ease/service/impl/SearchHistoryService.java
@@ -1,10 +1,12 @@
 package sk.fei.tp.ease.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import sk.fei.tp.ease.model.SearchHistory;
 import sk.fei.tp.ease.repository.SearchHistoryRepository;
 import sk.fei.tp.ease.service.ISearchHistoryService;
+import sk.fei.tp.ease.util.UserUtils;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -20,7 +22,7 @@ public class SearchHistoryService implements ISearchHistoryService {
         SearchHistory searchHistory = new SearchHistory();
         searchHistory.setTime(LocalDateTime.now());
         searchHistory.setSearchedExpression(query);
-        searchHistory.setUser("a");
+        searchHistory.setUser(UserUtils.getCurrentUser());
         searchHistoryRepository.save(searchHistory);
     }
 
