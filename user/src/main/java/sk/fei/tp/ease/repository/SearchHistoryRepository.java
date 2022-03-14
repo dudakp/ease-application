74e86337fdb64909b2426dcda47ad3307dcfdc25
Juraj Zozulak <zozulak.juraj@gmail.com>
2022-03-14 14:05:38 +0100

    job for old history deletion

--- user/src/main/java/sk/fei/tp/ease/repository/SearchHistoryRepository.java
+++ user/src/main/java/sk/fei/tp/ease/repository/SearchHistoryRepository.java
@@ -5,10 +5,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Modifying;
+import java.time.LocalDateTime;
 import sk.fei.tp.ease.model.SearchHistory;
 
 public interface SearchHistoryRepository extends PagingAndSortingRepository <SearchHistory, Long> {
 
     @Query("from SearchHistory sh where sh.user = :user")
     Page<SearchHistory> findByUser(@Param("user") String user, Pageable pageable);
+
+
+    @Modifying
+    @Query("delete from SearchHistory sh where time < :time")
+    void removeOldSearchHistory(@Param("time") LocalDateTime time);
 }
