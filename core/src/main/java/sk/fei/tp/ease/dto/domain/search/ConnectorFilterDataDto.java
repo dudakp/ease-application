85cb3e5ef213b8b28bfaf5e71f8a04cfaab746ef
dudakp <palodudak@gmail.com>
2022-05-07 21:27:48 +0200

    feat: farby pre hrany

--- core/src/main/java/sk/fei/tp/ease/dto/domain/search/ConnectorFilterDataDto.java
+++ core/src/main/java/sk/fei/tp/ease/dto/domain/search/ConnectorFilterDataDto.java
@@ -10,7 +10,7 @@ public class ConnectorFilterDataDto {
 
     public ConnectorFilterDataDto(ConnectorType connectorType, String stereotype) {
         this.connectorType = Map.of(connectorType.name(), String.format("#%06X", (0xFFFFFF & connectorType.name().length() + 5000)));
-        this.stereotype = Map.of(stereotype == null ? "" : stereotype, String.format("#%06X", (0xFF00FF & (stereotype == null ? 0 : stereotype.length() + 1000))));
+        this.stereotype = Map.of(stereotype == null ? "" : stereotype, String.format("#%06X", (0xFF0000 & (stereotype == null ? 0 : stereotype.length()))));
     }
 
     private final Map<String, String> connectorType;
