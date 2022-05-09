0493304227c674e44699aec541dd20630d9b78aa
dudakp <palodudak@gmail.com>
2022-05-09 20:40:36 +0200

    feat: farba

--- core/src/main/java/sk/fei/tp/ease/dto/domain/search/ConnectorFilterDataDto.java
+++ core/src/main/java/sk/fei/tp/ease/dto/domain/search/ConnectorFilterDataDto.java
@@ -10,7 +10,7 @@ public class ConnectorFilterDataDto {
 
     public ConnectorFilterDataDto(ConnectorType connectorType, String stereotype) {
         this.connectorType = Map.of(connectorType.name(), String.format("#%06X", (0xFFFFFF & connectorType.name().length() + 5000)));
-        this.stereotype = Map.of(stereotype == null ? "" : stereotype, String.format("#%06X", (0xFF0000 & (stereotype == null ? 0 : stereotype.length()))));
+        this.stereotype = Map.of(stereotype == null ? "" : stereotype, "#FF0000");
     }
 
     private final Map<String, String> connectorType;
